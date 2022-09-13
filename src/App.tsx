cabde9fa21423453ae4732006219c6e913d77b53
@s_zhsn <s.zhosan@gmail.com>
2022-09-13 21:17:02 +0300

    redux mechanism is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import {addItem, removeItem, editItem, toggleArchived} from "./redux/todosReducer";
+import { useSelector, useDispatch } from 'react-redux';
+import {addItem, removeItem, editItem, toggleArchived} from './redux/todosReducer';
 
-function App() {
-  const items = useSelector((state)=>state);
-  const dispatch = useDispatch();
-  useEffect(()=>{
-    console.log(items);
-    dispatch(toggleArchived("l28wmwom"));
-  },[items, dispatch]);
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
--- src/redux/todosReducer.ts
+++ src/redux/todosReducer.ts
@@ -1,20 +1,30 @@
 
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
-import { IToDo } from "../interfaces/interfaces";
+import { combineReducers, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+import { IToDo } from '../interfaces/interfaces';
 
 const initialState: IToDo[] = [{
-    "id": "l28wmwom",
-    "name": "Task #1",
-    "category": "task",
-    "categoryText": "Task",
-    "content": "12/10/2022 Content of task #1",
-    "created": "Septemper 10, 2022",
-    "isArchived": false,
-    "dates": ["12/10/2020"]
+    'id': 'l28wmwom',
+    'name': 'Task #1',
+    'category': 'task',
+    'categoryText': 'Task',
+    'content': '12/10/2022 Content of task #1',
+    'created': 'September 10, 2022',
+    'isArchived': false,
+    'dates': ['12/10/2020']
+  },
+  {
+    'id': 'l29wmwom',
+    'name': 'Task #2',
+    'category': 'task2',
+    'categoryText': 'Task',
+    'content': '15/10/2022 Content of task #1',
+    'created': 'September 10, 2022',
+    'isArchived': false,
+    'dates': ['12/10/2020']
   },];
 
-const toDos = createSlice({name:"items", initialState, reducers:{
+const toDos = createSlice({name:'toDoList', initialState, reducers:{
     addItem: {
         reducer: (state: IToDo[], {payload}: PayloadAction<IToDo>)=>[...state, payload],
         prepare: toDoItem=>{
@@ -33,11 +43,10 @@ const toDos = createSlice({name:"items", initialState, reducers:{
             item.category = payload.category;
             item.categoryText = payload.categoryText;
             item.content = payload.content;
-            item.dates = payload.dates?payload.dates:[""];
+            item.dates = payload.dates?payload.dates:[''];
         }
     },
     toggleArchived: (state: IToDo[], {payload}:PayloadAction<string>)=>{
-        console.log(state);
         const item = state.find(({id})=>id===payload);
         if(item){
             item.isArchived = !item.isArchived;
@@ -48,4 +57,4 @@ const toDos = createSlice({name:"items", initialState, reducers:{
 
 
 export const {addItem, removeItem, editItem, toggleArchived} = toDos.actions;
-export default toDos.reducer;
+export default combineReducers({toDoList: toDos.reducer});
